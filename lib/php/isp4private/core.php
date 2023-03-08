c391569afa3c148d3c41675505594f6c8bcb13f6
pimnik98 <nikita.piminoff@yandex.ru>
2023-03-08 14:57:38 +0300

    🥄 [I4PFunc] Добавлена функция для генерации паролей

--- lib/php/isp4private/core.php
+++ lib/php/isp4private/core.php
@@ -475,6 +475,17 @@ class I4PFunc{
 	public function filterStr($str){
 		return trim(htmlspecialchars($str));
 	}
+
+	# Генерируем пароль из n-символов
+	public function genPassword($number=8){
+		$arr = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','x','y','z', 'A','B','C','D','E','F','G','H','I','J','K','L', 'M','N','O','P','R','S', 'T','U','V','X','Y','Z','1','2','3','4','5','6','7','8','9','0');
+		$pass = "";
+		for($i = 0; $i < $number; $i++){
+			$index = rand(0, count($arr) - 1);
+			$pass .= $arr[$index];
+		}
+		return $pass;
+	}
 }
 
 ?>
